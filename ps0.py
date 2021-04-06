8563ffbeac79c849773831e9e1088eb8134cee82
Neha Pant <npant03@gmail.com>
2021-04-06 14:26:45 -0700

    added ps0 and ps1 to new repo

---
+++ ps0.py
@@ -0,0 +1,8 @@
+import math
+
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+product = x**y
+log = math.log(x, 2)
+print("x**y = " + str(product))
+print("log x base 2 = " + str(log))
---
+++ ps1.py
@@ -0,0 +1,21 @@
+# setting up variables
+total_cost = None # cost of dream home, will ask user
+portion_down_payment = 0.25 # must pay 25% of total cost upfront
+current_savings = 0
+r = 0.04 # rate of return on investment
+annual_salary = None # will ask user
+portion_saved = None # percentage of salary dedicated to down payment
+months = 0
+
+# asking for user input
+annual_salary = float(input("Enter your annual salary: $"))
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+total_cost = float(input("Enter the cost of your dream home: "))
+
+
+while(current_savings <= portion_down_payment * total_cost):
+    current_savings += current_savings * r / 12
+    current_savings += portion_saved*(annual_salary/12)
+    months += 1
+
+print("Number of months:", months)
